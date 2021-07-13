c7069a906972b3d461080234462d82aa54110391
Diego Montoya <dmontoyg@gmail.com>
2021-07-13 17:49:31 -0500

    primeros ejercicios js

---
+++ ejerDiaUno.js
@@ -0,0 +1,32 @@
+// Ejercicio 1
+    // let frase = prompt("Ingresa la frase deseada");
+    // console.log (frase);
+
+// Ejercicio 2
+    // let nombre = prompt("Ingresa tu nombre");
+    // console.log ("Hola " + nombre);
+
+// Ejercicio 3
+    // let numeroUno = prompt("Ingresa el primer número");
+    // let numeroDos = prompt("Ingresa el segundo número");
+    // var suma = parseInt(numeroUno) + parseInt(numeroDos);
+
+    // alert("La suma es: " + suma)
+    // console.log(suma);
+
+    // OTRA FORMA DE HACERLO
+    // let numeroUno = parseInt(prompt("Escriba el primer numero: "))
+    // let numeroDos = parseInt(prompt("Escriba el segundo numero: "))
+
+    // console.log("La suma es: " + `${numeroUno + numeroDos}`);
+    // alert("La suma es: " + `${numeroUno + numeroDos}`);
+
+// Ejercicio 4
+// let fechaNacimiento = prompt("Por favor ingresa tu año de nacimiento (AAAA)");
+// const anioActual = 2021;
+// var edad = anioActual - parseInt(fechaNacimiento);
+
+// alert("Tienes " + edad + " años");
+// console.log(edad);
+
+// Ejercicio 5
